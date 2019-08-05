c207f0757fa5ee004a89cbc96755208f10410824
PC-1 <alganiiqbal@gmail.com>
2019-08-06 01:05:40 +0700

    update

--- app/src/main/java/com/example/pc_1/akb/LoginActivity.java
+++ app/src/main/java/com/example/pc_1/akb/LoginActivity.java
@@ -1,13 +1,89 @@
 package com.example.pc_1.akb;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.prefs.Preferences;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText Edtuser,Edtpass;
+    Button Btnlogin,Btnregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Edtuser = findViewById(R.id.edt_username);
+        Edtpass = findViewById(R.id.edt_password);
+        Btnlogin = findViewById(R.id.btn_login);
+        Btnregister = findViewById(R.id.btn_register);
+        Btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String user = Edtuser.getText().toString();
+            String pass = Edtpass.getText().toString();
+
+            if(!cekUser(user)||!cekPassword(pass)){
+                Dialog();
+            }
+            else{
+                login();
+            }
+
+            }
+        });
+
+
+
+        Btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
+    private void login(){
+        Pref.setLoggedInUser(getBaseContext(),Pref.getRegisteredUser(getBaseContext()));
+        Pref.setLoggedInStatus(getBaseContext(),true);
+        startActivity(new Intent(getBaseContext(),MainActivity.class));finish();
+    }
+
+
+    private boolean cekPassword(String password){
+        return password.equals(Pref.getRegisteredPass(getBaseContext()));
+    }
+
+
+    private boolean cekUser(String user){
+        return user.equals(Pref.getRegisteredUser(getBaseContext()));
+    }
+
+    private void Dialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+        alertDialogBuilder.setTitle("Username atau Password anda salah !");
+        alertDialogBuilder
+                .setMessage("Silahkan Masukan Kembali username atau password yang benar!")
+                .setIcon(R.mipmap.ic_launcher)
+                .setCancelable(false)
+                .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        LoginActivity.this.finish();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 }
--- app/src/main/java/com/example/pc_1/akb/RegisterActivity.java
+++ app/src/main/java/com/example/pc_1/akb/RegisterActivity.java
@@ -1,13 +1,48 @@
 package com.example.pc_1.akb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText EdtUser,EdtPassword,EdtPasswordUlang;
+    Button BtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EdtUser = findViewById(R.id.edt_username);
+        EdtPassword = findViewById(R.id.edt_password);
+        EdtPasswordUlang = findViewById(R.id.edt_passwordulang);
+        BtnRegister = findViewById(R.id.btn_register);
+
+
+
+        BtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String User = EdtUser.getText().toString();
+                String password = EdtPassword.getText().toString();
+                String passwordulang = EdtPasswordUlang.getText().toString();
+                if(passwordulang.equals(password)){
+
+                    Pref.setRegisteredUser(getBaseContext(),User);
+                    Pref.setRegisteredPass(getBaseContext(),password);
+
+                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+                else{
+
+                }
+            }
+        });
+
     }
 }
